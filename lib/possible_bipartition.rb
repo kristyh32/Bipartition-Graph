4044407e02aab5524169daa63c8567eff29da75c
Kristy Hisaw <kristy.hisaw@gmail.com>
2020-04-08 16:36:50 -0700

    tests passing

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,38 @@
-
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.length < 2
+  
+  stack = []
+  visited = {}
+  
+  dislikes.each_index do |i|
+    if !visited[i]
+      stack << i
+      visited[i] = 0
+    end
+    
+    while !stack.empty?
+      curr_dog = stack.pop
+      disliked_dogs = dislikes[curr_dog]
+      
+      disliked_dogs.each do |dog|
+        if !visited[dog]
+          stack << dog
+          visited[dog] = number(visited[curr_dog])
+        elsif visited[dog] == visited[curr_dog]
+          return false
+        end
+      end
+    end
+    
+  end
+  
+  return true
 end
+
+def number(num)
+  if num == 0
+    return 1
+  else
+    return 0
+  end
+end	
